edc4d66aeb5a0d4946f117c756c9d56f88eff6d5
Peter Gao <petergao@berkeley.edu>
2013-11-21 11:32:58 -0800

    simple app

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -5,10 +5,8 @@ import org.apache.spark.SparkContext._
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "/home/peter/incubator-spark/README.md" // Should be some file on your system
-    val sc = new SparkContext(args(0), "Simple App", "/home/peter/incubator-spark/",
-        Seq("/home/peter/sqlStreaming/target/scala-2.9.3/sql-streaming_2.9.3-1.0.jar")
-        )
+    val logFile = "../incubator-spark/README.md" // Should be some file on your system
+    val sc = new SparkContext(args(0), "Simple App", System.getenv("SPARK_HOME"),  Seq("./target/scala-2.9.3/sql-streaming_2.9.3-1.0.jar"))
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
